5e61bf6735bd0115d2cf9d3ab4c319ed39ff042e
throwawaybean <59761801+throwawaybean@users.noreply.github.com>
2022-06-21 09:35:12 -0400

    Update download.ps1
    Changed the download address

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy remotesigned
 function downloadbuilder {
-Invoke-WebRequest -Uri "https://github.com/matstocks/PC-Build-Script/archive/master.zip" -OutFile "C:\build\PCBuild.zip"
+Invoke-WebRequest -Uri "https://github.com/throwawaybean/PC-Build-Script/archive/master.zip" -OutFile "C:\build\PCBuild.zip"
 Expand-Archive C:\build\PCBuild.zip -DestinationPath C:\build\
 }
 downloadbuilder
